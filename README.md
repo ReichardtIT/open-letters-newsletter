adc83a72af0f383d9aa723e5c6579f827c208159
Dominik Reichardt <dominik@reichardt-online.it>
2016-02-13 19:52:26 +0100

    umbau newsletter_form.view.php bootstrap Formular

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 #Open-Letters Newslettersystem
+
+Homepage: http://www.open-letters.de/
+Download: http://www.open-letters.de/download.html
+
+##Hinweis##
+Dieses Archiv beinhaltet Modifikationen und ist ein Test Archiv.
--- include/newsletter_form.view.php
+++ include/newsletter_form.view.php
@@ -11,8 +11,7 @@
 <?php
 echo("<form method=\"post\" action=\"" . filter_input(INPUT_SERVER, 'PHP_SELF') . "?sendit\">\n");
 ?>
-<table width="100%" cellspacing="0" cellpadding="0" border="0">
-
+<form class="form-horizontal">
     <?php
     if (isset($params['error'])) {
         echo("<tr>\n");
@@ -20,84 +19,61 @@ echo("<form method=\"post\" action=\"" . filter_input(INPUT_SERVER, 'PHP_SELF') 
         echo("</tr>\n");
     }
     ?>
-
-    <tr>
-        <td class="small">i18n(form_word_salutation)*</td>
-        <td class="small" style="padding-left:40px;">
-            <span class="small">
-                i18n(form_star_description)
-            </span>
-        </td>
-    </tr>
-
-    <tr>
-        <td class="small" valign="bottom">
-            <select name="title" style="margin-bottom:7px;">
-                <option value="">i18n(form_please_select)</option>
-                <option value="Frau" <?php if ($params['title'] == "Frau") echo "selected"; ?> >i18n(form_word_misses)</option>
-                <option value="Herr" <?php if ($params['title'] == "Herr") echo "selected"; ?> >i18n(form_word_mister)</option>
-            </select>
-        </td>
-        <td class="small">&nbsp;</td>
-    </tr>
-
-    <tr>
-        <td class="small">i18n(form_word_firstname)*</td>
-        <td class="small">i18n(form_word_lastname)*</td>
-    </tr>
-
-    <tr>
-        <td>
-            <input class="news" type="text" name="firstname" style="margin-bottom:7px;"
-            <?php
-            if (isset($params['firstname']))
-                echo "value=\"" . $params['firstname'] . "\" />";
-            else
-                echo "value=\"\" />"
-                ?>
-        </td>
-        <td>
-            <input class="news" type="text" name="name" style="margin-bottom:7px;"
-            <?php
-            if (isset($params['name']))
-                echo "value=\"" . $params['name'] . "\" />";
-            else
-                echo "value=\"\" />"
-                ?>
-        </td>
-    </tr>
-
-    <tr>
-        <td class="small">
-            i18n(form_word_mailaddress)*
-        </td>
-        <td class="small">&nbsp;</td>
-    </tr>
-
-    <tr>
-        <td>
-            <input class="news" type="text" name="newsemail" style="margin-bottom:7px;"
-            <?php
-            if (isset($params['newsemail']))
-                echo "value=\"" . $params['newsemail'] . "\" />";
-            else
-                echo "value=\"\" />"
-                ?>
-        </td>
-        <td>&nbsp;</td>
-    </tr>
-
-    <tr>
-        <td colspan="3" class="small">
-            <input type="checkbox" name="newsagreement" value="newsletter_notice_accepted" class="news" /> * i18n(form_newsagreement)
-        </td>
-    </tr>
-
-    <tr>
-        <td colspan="3" align="right">
-            <input type="hidden" name="sendit" value="1" />
-            <input class="news" type="submit" value="i18n(form_subscribe_submit)" />
-        </td>
-    </tr>
-</table>
+<section>
+  <div class="form-group">
+    <p class="bg-warning lead">i18n(form_star_description)</p>
+  </div>
+</section>
+<section>
+  <div class="form-group">
+    <label for="gender" class="control-label">i18n(form_word_salutation)</label>
+    <select name="title" id="gender" class="form-control">
+      <option value="">i18n(form_please_select)</option>
+	  <option value="Frau" <?php if ($params['title'] == "Frau") echo "selected"; ?> >i18n(form_word_misses)</option>
+	  <option value="Herr" <?php if ($params['title'] == "Herr") echo "selected"; ?> >i18n(form_word_mister)</option>
+    </select>
+  </div>
+</section>
+<section>
+  <div class="form-group">
+    <label for="firstname" class="control-label">i18n(form_word_firstname)*</label>
+    <input type="text" class="form-control" name="firstname"
+      <?php
+        if (isset($params['firstname']))
+        echo "value=\"" . $params['firstname'] . "\" />";
+        else
+        echo "value=\"\" />"
+      ?>
+  </div>
+</section>
+<section>
+  <div class="form-group">
+    <label for="name" class="control-label">i18n(form_word_lastname)*</label>
+    <input type="text" class="form-control" name="name"
+      <?php
+        if (isset($params['name']))
+        echo "value=\"" . $params['name'] . "\" />";
+        else
+        echo "value=\"\" />"
+      ?>
+  </div>
+</section>
+<section>
+  <div class="form-group">
+    <label for="newsemail" class="control-label">i18n(form_word_mailaddress)*</label>
+    <input type="email" class="form-control" name="newsemail" 
+      <?php
+        if (isset($params['newsemail']))
+        echo "value=\"" . $params['newsemail'] . "\" />";
+        else
+        echo "value=\"\" />"
+      ?>
+  </div>
+</section>
+<section>
+  <div class="form-group">
+    <input type="checkbox" name="newsagreement" value="newsletter_notice_accepted" class="news"> * i18n(form_newsagreement)
+  </div>
+</section>
+<button type="submit" class="btn btn-default btn-lg">i18n(form_subscribe_submit)</button>
 </form>
